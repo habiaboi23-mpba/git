d18431afa8c11f5d6e7d08a57e75d51719e444f9
Johannes Schindelin <johannes.schindelin@gmx.de>
2025-07-19 15:48:20 +0200

    fixup??? mingw: support long paths
    The `git_config_get_bool()` function is no more...
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- compat/mingw.c
+++ compat/mingw.c
@@ -295,7 +295,7 @@ int are_long_paths_enabled(void)
 
 		if (the_repository->config &&
 		    the_repository->config->hash_initialized &&
-		    git_config_get_bool("core.longpaths", &enabled) < 0)
+		    repo_config_get_bool(the_repository, "core.longpaths", &enabled) < 0)
 			enabled = 0;
 	}
 
